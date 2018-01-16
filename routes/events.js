21aa377bd820ddc21e6025342cdcff9d159e2eaf
Devin Weaver <suki@tritarget.org>
2018-01-16 10:18:38 -0500

    Add support for query params

--- routes/events.js
+++ routes/events.js
@@ -2,9 +2,24 @@ const Boom = require('boom');
 const express = require('express');
 const router = express.Router();
 const { wrap } = require('async-middleware');
+const paramGetter = require('../lib/param-getter');
+
+function eventsParams(query) {
+  let param = paramGetter(query);
+  return {
+    format: 'json',
+    sign: param('sign', true),
+    offset: param('offset', 0),
+    'limited_events': param('limited_events', 'False'),
+    page: param('page', 200),
+    order: param('order', 'time')
+  };
+}
 
 router.get('/', wrap(function(req, res) {
-  return req.meetup.getUpcomingEventsAsync({urlname: req.meetupUrlName})
+  let params = eventsParams(req.query);
+  params.urlname = req.meetupUrlName;
+  return req.meetup.getUpcomingEventsAsync(params)
     .catch(error => { throw Boom.badGateway(error); })
     .then(result => res.json(result));
 }));
--- routes/members.js
+++ routes/members.js
@@ -2,9 +2,23 @@ const Boom = require('boom');
 const express = require('express');
 const router = express.Router();
 const { wrap } = require('async-middleware');
+const paramGetter = require('../lib/param-getter');
+
+function membersParams(query) {
+  let param = paramGetter(query);
+  return {
+    sign: param('sign', true),
+    offset: param('offset', 0),
+    'photo-host': param('photo-host', 'public'),
+    page: param('page', 40),
+    order: param('order', 'name')
+  };
+}
 
 router.get('/', wrap(function(req, res) {
-  return req.meetup.getMembersAsync({'group_urlname': req.meetupUrlName})
+  let params = membersParams(req.query);
+  params['group_urlname'] = req.meetupUrlName;
+  return req.meetup.getMembersAsync(params)
     .catch(error => { throw Boom.badGateway(error); })
     .then(result => res.json(result));
 }));
